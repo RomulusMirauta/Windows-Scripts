c9bce0c610c7560546041620ca15fdd6c8b83b16
RomulusMirauta <romulus.mirauta1@gmail.com>
2024-09-20 18:00:29 +0300

    Testing with a .bat file3

--- TEST.bat
+++
@@ -1,9 +0,0 @@
-
-@echo off
-taskkill /f /im explorer.exe
-start explorer.exe
-
-
-REM Command parameters: 
-REM f = force (forces the command to be executed) 
-REM im = image name (of the process) 
