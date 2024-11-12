9f835863f1e07930bd5ab22d6ffc538646868f98
RomulusMirauta <romulus.mirauta1@gmail.com>
2024-11-12 20:18:31 +0200

    added .gitignore

--- PowerShell(Dis)MountDiskImageISO.ps1
+++ PowerShell(Dis)MountDiskImageISO.ps1
@@ -4,8 +4,17 @@
 PowerShell -Command "Mount-DiskImage -ImagePath '%path_to_your_file%\%file_name%.iso'"
 
 <# 
-Example: 'C:\Users\Romulus\Desktop\rad.iso'
-PowerShell -Command "Mount-DiskImage -ImagePath 'C:\Users\Romulus\Desktop\rad.iso'"
+Example: 'C:\Users\Romulus\Desktop\rad_folder\rad.iso'
+PowerShell -Command "Mount-DiskImage -ImagePath 'C:\Users\Romulus\Desktop\rad_folder\rad.iso'"
+#>
+
+
+
+<#
+*Additional: The variable '$PSScriptRoot' can be used in order to simplify the ImagePath - by avoiding the hardcoding of file paths - but using it requires that the .ps1 script is located in the same folder as the .iso file.
+'$PSScriptRoot' contains the full path to the directory of the script currently being run.
+Example: '$PSScriptRoot\rad_folder\rad.iso'
+PowerShell -Command "Mount-DiskImage -ImagePath '$PSScriptRoot\rad_folder\rad.iso'"
 #>
 
 
