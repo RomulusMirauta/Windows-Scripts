8218f33479d21e7be53c8e5e4f9b3026300f1f3b
RomulusMirauta <romulus.mirauta1@gmail.com>
2025-03-12 13:02:24 +0200

    updated GIT_COMMANDS.sh

--- GIT_COMMANDS.sh
+++ GIT_COMMANDS.sh
@@ -49,3 +49,33 @@ git status
 git add .
 git commit -m "updated README.md"
 git push
+
+
+
+# Clone a Repository
+
+git clone https://github.com/%Username%/%ProjectName%.git
+
+
+
+#Contributing To GitHub Projects - example
+
+git clone https://github.com/RomulusMirauta/markdown-badges
+cd markdown-badges
+git remote add upstream https://github.com/Ileriayo/markdown-badges
+git checkout -b adding-new-badges
+_MAKE MODS LOCALLY_
+git status
+git add .
+git commit -m "Added badge for Hearthstone Collection"
+--git push origin added-badge-hearthstone-collection
+git push origin adding-new-badges
+_GitHub Project - Create Pull Request - WAIT_
+
+_MODS NEEDED?_
+_MAKE MODS LOCALLY_
+git status
+git add .
+git commit -m "Updated code for Hearthstone Collection badge"
+git push origin adding-new-badges
+_GitHub Project - Create Pull Request - WAIT_
