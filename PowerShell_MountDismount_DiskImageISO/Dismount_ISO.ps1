06c8f44e73d1d059212aa697050da9480be34946
RomulusMirauta <romulus.mirauta1@gmail.com>
2025-03-12 15:14:43 +0200

    cleaning-up

---
+++ PowerShell_MountDismount_DiskImageISO/Dismount_ISO.ps1
@@ -0,0 +1,11 @@
+
+# DISMOUNT ISO IMAGE
+
+Dismount-DiskImage -ImagePath "$PSScriptRoot\rad.iso"
+
+
+# Or just right-click on the created Drive and select Eject
+
+
+# For Debugging
+# Read-Host -Prompt "Press Enter to close this window"
---
+++ PowerShell_MountDismount_DiskImageISO/Mount_ISO.ps1
@@ -0,0 +1,8 @@
+
+# MOUNT ISO IMAGE
+
+PowerShell -Command "Mount-DiskImage -ImagePath '$PSScriptRoot\rad.iso'"
+
+
+# For Debugging Purposes - uncomment the following line of code
+# Read-Host -Prompt "Press Enter to close this window"
