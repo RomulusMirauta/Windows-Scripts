884a6800f4f2a1c52d02fb7aa03816e5cdd4c1c2
RomulusMirauta <romulus.mirauta1@gmail.com>
2026-03-03 09:36:46 +0200

    Update video codec options to use consistent casing for improved readability

--- POWERSHELL+/ffmpeg/FFmpegVideoAspectRatioResizer_v1.1.ps1
+++ POWERSHELL+/ffmpeg/FFmpegVideoAspectRatioResizer_v1.1.ps1
@@ -420,7 +420,7 @@ foreach ($i in 0..($fpsOptions.Count-1)) {
 Show-ParameterOptions "Frame rate" $fpsOptions $fpsDisplay $fpsCurrentIdx
 
 # Video Codec display (sorted alphabetically)
-$codecOptions = @("av1", "h264", "h265", "mpeg2video", "mpeg4", "vp9", "prores")
+$codecOptions = @("AV1", "H.264", "H.265 (HEVC)", "mpeg2video", "mpeg4", "VP9", "ProRes")
 $codecDisplay = @()
 foreach ($opt in $codecOptions) {
     if ($videoCodec -eq $opt) {
